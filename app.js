38aea9174918815522e8b7dffae774d5e5f899f7
Jon Lambson <jon.lambson@daplie.com>
2017-10-02 22:28:27 -0600

    mail server setup

--- app.js
+++ app.js
@@ -28,14 +28,27 @@ app.post('/contact/send', (req, res) => {
   var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'jonlambson@gmail.com',
+      user: 'blah@gmail.com',
       pass: ''
     }
   });
   var mailOptions = {
-    from: 'Dude Face <jonlambson@gmail.com>',
-    to: ''
-  }
+    from: 'Jon Lambson <blah@gmail.com>',
+    to: 'blah@gmail.com',
+    subject: 'Website Submission',
+    text: 'blah blah blah blah. Name: ' + req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+    html: '<p>asdflkjasd asdf asdfjasdk lasdf</p>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message Sent: ', info.response);
+      res.redirect('/');
+    }
+  });
 });
 
 app.listen(3000);
